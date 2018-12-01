23094749d37e0cd9cc67f9fcbc8a486e4aaa72be
Thiago Rodrigues <bulfaitelo@gmail.com>
2018-12-01 01:16:26 -0200

    home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<!DOCTYPE html>
+<html lang="en">
+
+  <head></head>
+  <meta charset="utf-8">
+   
+<div class="container">
+<h3>Oficina</h3>
+</div>
+@endsection
