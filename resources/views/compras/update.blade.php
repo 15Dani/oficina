24cf99904e30c69847a94931f154ff840af6d3ef
Unknown <danielia.de@bol.com.br>
2018-12-01 15:35:01 -0200

    bla bla bla comit

--- resources/views/compras/update.blade.php
+++ resources/views/compras/update.blade.php
@@ -9,26 +9,26 @@
    
 <div class="container">
      
-        <form method="POST" action="{{ route('compras.update', $compra->id) }}">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}"> 
-                <input name="_method" type="hidden" value="PUT">             
-                    <div class="form-group">
-                          <label for="nome"> Nome da Compras </label>
-                    <input value="{{$compra->nome}}" class ="form-control" type="text" id="nome" name="nome" placeholder="Nome"/>
-                   </div>
-                   <div class="form-group"> 
-                       <label for="valor"> Valor da compra</label>
-                       <input value="{{$compra->valor}}" class ="form-control" type="text" id="valor" name="valor" placeholder="Valor"/>
-                    </div>     
-                   
-                   
-                     <! CRuD formulario -->
-                   <div class="form-group">
-                       <input class="btn btn-success" type="submit" value="Salvar" />                       
-       
-                     </div>
-                       
-                   </form>  
+        <form method="POST" action="{{ route('compras.update', $compras->id) }}">
+          <input type="hidden" name="_token" value="{{ csrf_token() }}"> 
+          <input name="_method" type="hidden" value="PUT">           
+            <div class="form-group">
+                <label for="nome"> Nome da compra </label>
+                <input  value="{{ $compras->nome }}"  class ="form-control" type="text" id="nome" name="nome" placeholder="Nome"/>
+             </div>
+             <div class="form-group"> 
+                 <label for="valor"> valor da compra</label>
+                 <input class ="form-control" type="int" id="valor" name="valor" value="{{ $compras->valor }}"  placeholder="valor"/>
+              </div>    
+                        
+             
+               <! CRuD formulario -->
+             <div class="form-group">
+                 <input class="btn btn-success" type="submit" value="Salvar" />                       
+  
+               </div>
+                 
+        </form>  
 
 </div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,49 @@
 @section('content')
 <!DOCTYPE html>
 <html lang="en">
+<meta charset="utf-8">
+
+<!-- Css do menu do sistema -->
 
   <head></head>
-  <meta charset="utf-8">
-   
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  </head>
+  <body>
+  <br>  
+  
 <div class="container">
-    {!! $MyNavBar->asDiv() !!}    
+    {!! $MyNavBar->asDiv() !!}   
+    <h2>Sistema de Gestão</h2>
+     <br>
+     <!-- chamado os menus com as telas -->
+     <ul class="nav nav-tabs" role="tablist">
+     <li class="nav-item">
+     <a class="nav-link active" data-toggle="tab" href="#home">Home</a>
+     </li>
+     <li class="nav-item">
+     <a class="nav-link"  data-toggle="tab" href="#cliente">Cliente</a>
+     </li>
+     <li class="nav-item">
+     <a class="nav-link" data-toggle="tab" href="#veiculo">Veiculo</a>
+     </li>
+     <li class="nav-item">
+      <a class="nav-link" data-toggle="tab" href="#Funcionário">Funcionário</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="tab" href="#Fornecedor">Fornecedor</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="tab" href="#Produto">Produto</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="tab" href="#Compra">Compra</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="tab" href="#Pagamento">Pagamento </a>
+    </li>
+    </ul>
 </div>
 @endsection
