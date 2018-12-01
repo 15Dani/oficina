581fc754e4707924a8c684d76254203ac0288e44
Thiago Rodrigues <bulfaitelo@gmail.com>
2018-11-30 23:01:19 -0200

    menu install

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,11 +49,24 @@
                                 @endif
                             </li>
                         @else
+                        <div class="dropdown">
+                                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                  Funções
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    {!! $MyNavBar->asUl() !!}                                  
+                                </div>
+                              </div>
+                        <li class="nav-item dropdown">
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            </div>
+                        </li>
                             <li class="nav-item dropdown">
+                                
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
-
+                                
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
