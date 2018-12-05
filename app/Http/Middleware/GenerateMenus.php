7cdc6ee61800e0a774f127aa02d55279c5a5d003
Thiago Rodrigues <bulfaitelo@gmail.com>
2018-12-04 23:40:27 -0200

    role add

--- app/Http/Middleware/GenerateMenus.php
+++ app/Http/Middleware/GenerateMenus.php
@@ -15,15 +15,23 @@ class GenerateMenus
      */
     public function handle($request, Closure $next)
     {
-        \Menu::make('MyNavBar', function ($menu) {
-            $menu->add('Cliente', 'clientes');
+        \Menu::make('MyNavBar', function ($menu) {            
+            if(\Auth::user()!=null){
+                if(\Auth::user()->role == 0){
+                    $menu->add('Cliente', 'clientes');
+                    $menu->add('Fornecedor', 'fornecedores');
+                    $menu->add('Funcionário', 'funcionarios');
+                    $menu->add('Veículo', 'veiculos');
+                    $menu->add('Produto', 'produtos');
+                    $menu->add('Compra', 'compras');
+                    $menu->add('Pagamento', 'contact');                    
+                }
+                if(\Auth::user()->role >= 2){                    
+                    $menu->add('Veículo', 'veiculos');
+                    $menu->add('Produto', 'produtos');                  
+                }
 
-            $menu->add('Fornecedor', 'fornecedores');
-            $menu->add('Funcionário', 'funcionarios');
-            $menu->add('Veículo', 'veiculos');
-            $menu->add('Produto', 'produtos');
-            $menu->add('Compra', 'compras');
-            $menu->add('Pagamento', 'contact');
+            }
 
         });
 
