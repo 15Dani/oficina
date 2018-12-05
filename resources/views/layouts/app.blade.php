b46bf356941347a8eb5802ad39353be27a6b07ed
Thiago Rodrigues <bulfaitelo@gmail.com>
2018-12-04 23:44:00 -0200

    home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,10 +22,7 @@
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/home') }}">
-                    Oficina
-                </a>
+            <div class="container">                
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
